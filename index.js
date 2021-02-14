d525cc2175e9123170c74025d5c41e9a4bae8742
rebecca <rebecca.alex.maie@gmail.com>
2021-02-14 17:09:12 -0500

    a working dropdown menu, still a little ugly though

---
+++ index.js
@@ -0,0 +1,19 @@
+// I worked on this code with my pervious partner. There will be some similarities. 
+
+
+
+
+
+let burger = document.getElementById('burgerID');
+let burgerNav = document.getElementById("jsMenu");
+
+
+
+burger.addEventListener('click', function(event){
+    burgerNav.classList.toggle('active');
+    // burger.classList.toggler('burgerDisplay');
+    console.log("click");
+ //on click we want the 
+ //on click we want the 
+});
+
