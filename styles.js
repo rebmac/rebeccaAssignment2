ed31c8e48054ec34144d05ba3b35ea5283f01fe1
rebecca <rebecca.alex.maie@gmail.com>
2021-02-15 16:47:53 -0500

    made the form element alert when empty

--- styles.js
+++ styles.js
@@ -17,3 +17,33 @@ burger.addEventListener('click', function(event){
  //on click we want the 
 });
 
+// make it so the form needs content to submit
+// if nothing inside alert"fill me out first"
+
+const formElement = document.querySelector('form');
+formElement.addEventListener('submit', function(){
+    event.preventDefault();
+    // alert("beep");
+    const nameInputElement = document.getElementById('name');
+    const emailInputElement = document.getElementById('email');
+    const textAreaInputElement = document.getElementById('textArea');
+
+    if (nameInputElement.value === ""){
+        alert("Please fill out your name.");
+    };
+
+    if (emailInputElement.value === ""){
+        alert("Please fill out your email.");
+    };
+
+    if (textAreaInputElement.value === ""){
+        alert("Please fill out your comment.");
+    };
+    nameInputElement.value = '';
+    emailInputElement.value = '';
+    textAreaInputElement.value = '';
+});
+
+
+
+
